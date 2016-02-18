03f5bda46c869412120b24adf0279102ca9d37d3
jonathan.haywood@gmail.com <jonathan.haywood@gmail.com>
2016-02-17 17:18:39 -0700

    Added convenience extension methods.

---
+++ source/ObjectMapper/ObjectMapperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ObjectMapper.TypeConverters;
+
+namespace ObjectMapper
+{
+    /// <summary>
+    /// Convenience extensions methods for ObjectMapper.
+    /// </summary>
+    public static class ObjectMapperExtensions
+    {
+        public static IObjectMapperInstance AddNullableConverters(this IObjectMapperInstance objectMapper)
+        {
+            objectMapper.AddConverter(new NullableToValue(), new ValueToNullable());
+            return objectMapper;
+        }
+
+        public static IObjectMapperInstance AddListAndArrayConverters(this IObjectMapperInstance objectMapper)
+        {
+            objectMapper.AddConverter(new ListToArray(), new ArrayToList());
+            return objectMapper;
+        }
+    }
+}
